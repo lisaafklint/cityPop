d76ffb53b4e848d8ca06aea5681cf4f43245bddc
Lisa af Klint <li6285fr-s@student.lu.se>
2022-02-03 10:53:58 +0100

    Implemented home screen

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, TouchableHighlight, Button, Pressable} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.textContainer}>
+        <Text CityPop style={styles.title}>
+          CityPop
+        </Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Pressable 
+          onPress={() => {}} >
+          <Text style={styles.buttonText}> SEARCH BY CITY </Text>
+        </Pressable>
+
+        <Pressable 
+          onPress={() => {}} >
+          <Text style={styles.buttonText}> SEARCH BY COUNTRY </Text>
+        </Pressable>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
+    backgroundColor: 'grey',
+    alignItems: 'center',
   },
+  textContainer: {
+    flex: 3,
+/*     backgroundColor: "green",
+ */    justifyContent: 'center'
+  },
+  buttonContainer: {
+    flex:6,
+/*     backgroundColor: "red",
+ */ flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: 'black'
+  }
 });
